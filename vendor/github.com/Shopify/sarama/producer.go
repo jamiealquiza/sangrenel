8f8bbc004a6f30173707836671f4dc7955e61808
Jamie Alquiza <ja@grey-boundary.io>
2015-01-31 17:43:04 -0700

    locally manages Sarama dep eapache/go/go-resiliency/breaker from commit 5b8ad6c0f61096fdfca79f5e2226c73bb031ef87

--- vendor/github.com/Shopify/sarama/producer.go
+++ vendor/github.com/Shopify/sarama/producer.go
@@ -5,7 +5,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/eapache/go-resiliency/breaker"
+	"github.com/jamiealquiza/sangrenel/vendor/github.com/eapache/go-resiliency/breaker"
 )
 
 func forceFlushThreshold() int {
